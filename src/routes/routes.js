1f97ef2db8bb58aa9354e80b984a4b2b24dfe23a
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-10-06 11:14:44 +0500

    add package

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,7 @@
 const homeRoute = require("../routes/homeRoute");
 const userRoute = require("../routes/userRoute");
 
-
-module.exports = (app) =>{
-    app.use(homeRoute.path, homeRoute.router);
-    app.use(userRoute.path, userRoute.router);
+module.exports = (app) => {
+  app.use(homeRoute.path, homeRoute.router);
+  app.use(userRoute.path, userRoute.router);
 };
