4a06e17e8e24ed13a484675f36bc86279c0c521b
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-10-13 12:19:50 +0500

    add socket dars 1-si  30min

--- src/controllers/messagesController.js
+++ src/controllers/messagesController.js
@@ -1,77 +1,106 @@
 const users = require("../models/userModels");
-const {isValidObjectId} = require("mongoose");
-const messages =  require("../models/messageModel");
-const { $where} = require("../models/messageModel");
-const { messageValidation } = require("../modules/validations");
+const {
+  isValidObjectId
+} = require("mongoose");
+const messages = require("../models/messageModel");
+const {
+  $where
+} = require("../models/messageModel");
+const {
+  messageValidation
+} = require("../modules/validations");
 
 module.exports = class messagesRouteController {
- 
-  static async messagesGetController(req, res) {
 
-    try{
-      const isValidId =  isValidObjectId(req.params?.id);
-      if(!isValidId) throw new Error("Invalid");
-      const receiver_id = await users.findOne({
-        _id:req.params.id,
-      })
-      // console.log(receiver_id);
+    static async messagesGetController(req, res) {
 
-      if(!receiver_id) throw new Error("User not found");
+      try {
+        const isValidId = isValidObjectId(req.params?.id);
+        if (!isValidId) throw new Error("Invalid");
+        const receiver_id = await users.findOne({
+          _id: req.params.id,
+        })
+        // console.log(receiver_id);
+
+        if (!receiver_id) throw new Error("User not found");
 
 
-      const chats = await messages.find({
-        $or:[
-         {
-           $and:[
-             {
-              owner_id:req.user._id,
-             },{
-              receiver_id:receiver_id._id,
-             }
-           ],
-          },
-          {
-           $and:[
+        const chats = await messages.find({
+          $or: [{
+              $and: [{
+                owner_id: req.user._id,
+              }, {
+                receiver_id: receiver_id._id,
+              }],
+            },
             {
-              receiver_id:req.user._id,
+              $and: [{
+                receiver_id: req.user._id,
 
-            },{
-              owner_id:receiver_id._id,
+              }, {
+                owner_id: receiver_id._id,
+              }]
             }
           ]
-         }
-        ]
-      }).sort([["created_at",1]]);
-     
-      
-      res.render("messages",{
-        user:req.user,
-        receiver:receiver_id,
-        chats,
-      });
+        }).sort([
+          ["created_at", 1]
+        ]);
 
-    }
-      catch(error){
+        // console.log(chats);
+
+        res.render("messages", {
+          user: req.user,
+          receiver: receiver_id,
+          chats,
+        });
+
+      } catch (error) {
         console.log(error)
         res.redirect("/");
       }
     }
 
 
-    static async messagesPostController(req,res){
-      try{
-const {message_text } = await messageValidation(req.body);
+    static async messagesPostController(req, res) {
+      try {
+        const 
+       { message_text}
+         = await messageValidation(req.body);
 
-if(!(isValidObjectId(req.user._id) && isValidObjectId(req.params?.id))
-      }
-      catch(error){
-        console.log(error);
-      }
-    }
-
-
-    // res.render("messages",{
-    //   user:req.user,
-    // })
+        if (
+          !(
+            isValidObjectId(req.user._id) &&
+            isValidObjectId(req.params?.id)
+          )
+        )
+          throw new Error("Id invalid");
   
-};
+          const chat = await messages.create({
+            message_text:message_text,
+            owner_id:req.user._id,
+            receiver_id:req.params?.id,
+
+          })
+
+          res.json({
+            ok:true,
+          })
+          // console.log(chat)
+          // res.redirect("/messages/"+req.params.id);
+        }
+        catch (error) {
+          console.log(error);
+          res.json({
+            ok:false, 
+            message:error+""
+          })
+          // res.redirect("/messages/"+req.params.id);
+        }
+      }
+
+
+      // res.render("messages",{
+      //   user:req.user,
+      // })
+
+    };
